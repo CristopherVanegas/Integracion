dc4392de1332746a6a260267a1e3da299292be06
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-08-31 11:58:13 -0500

    Installed bootstrap and added component tabla-usuarios

---
+++ integracion/src/app/components/tabla-usuarios/tabla-usuarios.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-usuarios',
+  templateUrl: './tabla-usuarios.component.html',
+})
+export class TablaUsuariosComponent {
+
+}
